cc951aec9f8d1df93d1dd35d1c30c7f15e64dc09
Dan Harper <intouch@danharper.me>
2016-01-18 15:59:47 +0000

    store MessageBag on Invalid for possible future access

--- src/Invalid.php
+++ src/Invalid.php
@@ -1,6 +1,5 @@
 <?php namespace Radweb\Tumble;
 
-
 use Illuminate\Contracts\Support\MessageBag;
 use Illuminate\Contracts\Support\MessageProvider;
 
@@ -9,23 +8,37 @@ class Invalid extends BadRequest {
 	protected $message = 'Invalid Request';
 
 	/**
+	 * @var MessageBag|null
+	 */
+	protected $bag;
+
+	/**
 	 * @param MessageBag|MessageProvider|string[] $errors
 	 */
 	public function __construct($errors = [])
 	{
 		parent::__construct();
 
+		if ($errors instanceof MessageProvider)
+		{
+			$errors = $errors->getMessageBag();
+		}
+
 		if ($errors instanceof MessageBag)
 		{
+			$this->bag = $errors;
 			$errors = $errors->all();
 		}
 
-		if ($errors instanceof MessageProvider)
-		{
-			$errors = $errors->getMessageBag()->all();
-		}
-
 		$this->details = $errors;
+	}
+
+	/**
+	 * @return MessageBag|null
+	 */
+	public function getBag()
+	{
+		return $this->bag;
 	}
 
 }
