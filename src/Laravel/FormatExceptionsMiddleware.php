5cbf9cfe7d304429d3b31961729b66f16f66d9ba
Dan Harper <intouch@danharper.me>
2016-01-25 12:56:00 +0000

    Laravel 5.2 support
    It no longer throws exceptions through middleware, but instead returns a
    valid response with the exception set on it
    
    see: https://github.com/lucadegasperi/oauth2-server-laravel/pull/653

--- src/Laravel/FormatExceptionsMiddleware.php
+++ src/Laravel/FormatExceptionsMiddleware.php
@@ -16,7 +16,15 @@ class FormatExceptionsMiddleware {
 	{
 		try
 		{
-			return $next($request);
+			$response = $next($request);
+
+			// Laravel 5.2 doesn't throw exceptions, it returns responses with it included
+			if (isset($response->exception) && $response->exception)
+			{
+				throw $response->exception;
+			}
+
+			return $response;
 		}
 		catch (\Exception $e)
 		{
